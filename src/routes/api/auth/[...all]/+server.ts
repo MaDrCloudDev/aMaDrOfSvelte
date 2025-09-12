9aa1795c1aede9db7c0ddc07950156750da65cc9
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-09-11 23:04:00 -0700

    test production fixes
    test production fixes

--- src/routes/api/auth/[...all]/+server.ts
+++ src/routes/api/auth/[...all]/+server.ts
@@ -1,5 +1,10 @@
-import { auth } from '$lib/server/auth';
-import type { RequestHandler } from './$types';
+import { auth } from '$lib/server/auth.js';
+import type { RequestHandler } from './$types.js';
 
-export const GET = auth.handler;
-export const POST = auth.handler;
+export const GET: RequestHandler = async (event) => {
+	return auth.handler(event.request);
+};
+
+export const POST: RequestHandler = async (event) => {
+	return auth.handler(event.request);
+};
